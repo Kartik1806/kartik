6ff1425b42ae02a2a41d892d7ed6cce20ad9e76b
Kartik1806 <113456468+Kartik1806@users.noreply.github.com>
2022-09-15 10:35:56 +0530

    Add files via upload

---
+++ datemonthyear.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a day");
+    scanf("%d",&a);
+    printf("Enter a month");
+    scanf("%d",&b);
+    printf("Enter a year");
+    scanf("%d",&c);
+    printf("Day %d /Month %d /Year %d",a,b,c);
+    return 0;
+}
